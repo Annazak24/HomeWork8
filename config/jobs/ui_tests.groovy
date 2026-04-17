88846fbb1a62a67f37248302d0563e4bddd715c5
Annazak24 <zaqaryan.anna856@gmail.com>
2026-04-17 22:10:07 +0400

    homework 8

--- config/jobs/ui_tests.groovy
+++ config/jobs/ui_tests.groovy
@@ -1,11 +1,38 @@
-node('built-in') {
+node('maven') {
 
     stage('Checkout') {
         checkout scm
     }
 
-    stage('Run UI Tests') {
-        sh 'mvn clean test'
+    stage('Prepare config') {
+        writeFile file: 'config.yaml', text: params.CONFIG
+
+        env.BROWSER = sh(
+                script: "grep '^browser:' config.yaml | cut -d':' -f2- | xargs",
+                returnStdout: true
+        ).trim()
+
+        env.BASE_URL = sh(
+                script: "grep '^base_url:' config.yaml | cut -d':' -f2- | xargs",
+                returnStdout: true
+        ).trim()
+
+        env.REMOTE = sh(
+                script: "grep '^remote:' config.yaml | cut -d':' -f2- | xargs",
+                returnStdout: true
+        ).trim()
+
+        echo "Browser: ${env.BROWSER}"
+        echo "Base URL: ${env.BASE_URL}"
+        echo "Remote: ${env.REMOTE}"
     }
 
+    stage('Run UI Tests') {
+        sh """
+            mvn clean test \
+              -Dbrowser=${env.BROWSER} \
+              -Dbase.url=${env.BASE_URL} \
+              -Dremote=${env.REMOTE}
+        """
+    }
 }
