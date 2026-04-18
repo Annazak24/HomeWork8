394f9216519867af3b48a46e8ba06cf9ee8aa392
Annazak24 <zaqaryan.anna856@gmail.com>
2026-04-18 20:31:46 +0400

    change uploader groovy

--- config/jobs/jobs_uploader.groovy
+++ config/jobs/jobs_uploader.groovy
@@ -1,4 +1,4 @@
-node('maven') {
+node('built-in') {
 
     def JOBS_DIR = "${env.WORKSPACE}/config/jobs"
     def CONFIG_FILE = "${env.WORKSPACE}/uploader_config.ini"
@@ -13,41 +13,31 @@ node('maven') {
                 passwordVariable: 'pass',
                 usernameVariable: 'user'
         )]) {
-            withEnv([
-                    "CONFIG_FILE=${CONFIG_FILE}",
-                    "JOBS_DIR=${JOBS_DIR}"
-            ]) {
-                sh '''
-                    cat > "$CONFIG_FILE" <<EOF
+            sh """
+                cat > "${CONFIG_FILE}" <<EOF
 [jenkins]
 url=http://jenkins:8080/my_jenkins/
-user=$user
-password=$pass
+user=${user}
+password=${pass}
 
 [job_builder]
 recursive=True
 keep_descriptions=False
 EOF
-                '''
-            }
+            """
         }
     }
 
     stage('Run JJB in Docker') {
-        withEnv([
-                "CONFIG_FILE=${CONFIG_FILE}",
-                "JOBS_DIR=${JOBS_DIR}"
-        ]) {
-            script {
-                docker.image('jenkins-jjb:latest').inside {
-                    sh '''
-                        jenkins-jobs --version
-                        ls -la
-                        ls -la "$JOBS_DIR"
-                        cat "$CONFIG_FILE"
-                        jenkins-jobs --conf "$CONFIG_FILE" --flush-cache update "$JOBS_DIR"
-                    '''
-                }
+        script {
+            docker.image('jenkins-jjb:latest').inside('--network selenoid1') {
+                sh """
+                    jenkins-jobs --version
+                    ls -la
+                    ls -la "${JOBS_DIR}"
+                    cat "${CONFIG_FILE}"
+                    jenkins-jobs --conf "${CONFIG_FILE}" --flush-cache update "${JOBS_DIR}"
+                """
             }
         }
     }
