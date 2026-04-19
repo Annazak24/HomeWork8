4f1d2eb5ada53abdb4cfa53f9ee75771f763430f
Annazak24 <zaqaryan.anna856@gmail.com>
2026-04-19 14:33:32 +0400

    clean

--- src/main/java/factory/settings/ISettings.java
+++
@@ -1,8 +0,0 @@
-package factory.settings;
-
-import org.openqa.selenium.remote.AbstractDriverOptions;
-
-public interface ISettings {
-
-    AbstractDriverOptions settings();
-}
--- src/main/resources/user.feature
+++
@@ -1,6 +0,0 @@
-Feature: User API
-
-  Scenario: Get user list
-    Given stub server is running
-    When I request user list
-    Then response status should be 200
